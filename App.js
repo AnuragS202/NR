5c3dd9245396676d5cd0d89d347d2b8dcb9fea39
Anurag Singh <anurag.singh202@gmail.com>
2023-10-23 15:20:11 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Jsx (transpiled before it reaches the js)  - Parcel - babel does the work
+//*NOTE:-  Jsx (transpiled before it reaches the js)  - Parcel - babel does the work
+//*NOTE:- Jsx => Babel transpiles it to React.CreateElement => ReactElement.Js Object => HTmlElement(render)
 
-// Jsx => Babel transpiles it to React.CreateElement => ReactElement.Js Object => HTmlElement(render)
-
-const jsxHeading = <h1 className="head">Hello World 👍</h1>;
-const jsxHeading1 = (
+// !React Element
+const Title = () => (
   <h1 className="head" tabIndex="5">
     Hello World 👍
   </h1>
 );
 
+// !React Functional Component
+// *NOTE:- component composition - Composing two component into one another . ex- here we are calling <Title/> component into heading component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">
+      This is a example of React Functional Component{" "}
+    </h1>
+  </div>
+);
 
-console.log(jsxHeading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponen />);
