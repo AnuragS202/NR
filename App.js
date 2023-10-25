6b5a61f460f67ae5c636b097f93a988ab8fe56cb
Anurag Singh <anurag.singh202@gmail.com>
2023-10-25 22:29:14 +0530

    Episode 4 1st half- creating 1st card

--- App.js
+++ App.js
@@ -1,27 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//*NOTE:-  Jsx (transpiled before it reaches the js)  - Parcel - babel does the work
-//*NOTE:- Jsx => Babel transpiles it to React.CreateElement => ReactElement.Js Object => HTmlElement(render)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtuRv1BBLHTuArUiwH-PGe8_i2oG3iRHFY8w&usqp=CAU"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// !React Element
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Hello World 👍
-  </h1>
-);
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="resturant-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/89fccaa76f2f760e2742b9e53d32bb69"
+      />
+      <h4>Super Food</h4>
+      <h5>Biryani, Asian, Continental </h5>
+      <h5>4.1 stars</h5>
+      <h5>30 mins ETA</h5>
+    </div>
+  );
+};
 
-// !React Functional Component
-// *NOTE:- component composition - Composing two component into one another . ex- here we are calling <Title/> component into heading component
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading">
-      This is a example of React Functional Component{" "}
-    </h1>
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="Rest-container">
+        <ResturantCard />
+      </div>
+    </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponen />);
+root.render(<Applayout />);
