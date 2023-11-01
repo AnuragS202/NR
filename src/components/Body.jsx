8a09064bfa02c4a698595d8bf740b1550d7fba1a
Anurag Singh <anurag.singh202@gmail.com>
2023-11-01 13:55:03 +0530

    Episode 5 fixes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import resList from "../../utils/mockdata";
 import ResturantCard from "./ResturantCard";
 
 const Body = () => {
+  const [listOfResturants, setListOfResturants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="Search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfResturants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfResturants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfResturants.map((restaurant) => (
           <ResturantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
